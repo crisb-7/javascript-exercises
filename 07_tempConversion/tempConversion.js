5f8ca830f463299809ef462e3558e22986531345
crisb-7 <cristofer.becerra@protonmail.com>
2023-04-01 16:51:59 -0600

    Exercise 6 complete, start of exercise 7.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celsius;
+  celsius = (fahrenheit - 32)*(5/9);
+  // celsius = Math.round(celsius*10)/10;
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahrenheit;
+  fahrenheit = celsius*(5/9) + 32;
+  // fahrenheit = Math.round(fahrenheit*10)/10
+  return fahrenheit;
 };
 
 // Do not edit below this line
