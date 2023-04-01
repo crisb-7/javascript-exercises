1e24bde14fb50138e5c8cf2f08785f2aa5516752
crisb-7 <cristofer.becerra@protonmail.com>
2023-03-31 21:22:19 -0600

    First 4 exercises done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringLength = string.length;
+    let reversedString = "";
+    for (let i = 0; i < stringLength; i++) {
+        reversedString += string.at(stringLength - 1 - i);
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...restArgs) {
+    let newArray = [];
+    for (let i=0; i<array.length; i++) {
+        let item = array[i];
+        let condition = restArgs.includes(item);
+        if (condition) {
+            continue;
+        } else {
+            newArray.push(item);
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
