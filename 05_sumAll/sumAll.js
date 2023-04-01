00bda856142729644be42dd2a4cdd296c8aab9b9
crisb-7 <cristofer.becerra@protonmail.com>
2023-03-31 22:02:58 -0600

    Exercise 5 complete.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum, secondNum) {
+    if (firstNum < 0 || secondNum < 0) {
+        return "ERROR";
+    } else if (typeof firstNum === "number" && typeof secondNum === "number") {
+        let sortNumbers = [firstNum, secondNum];
+        sortNumbers = sortNumbers.sort();
+        let start = sortNumbers[0];
+        let end = sortNumbers[1];
+        sum = 0;
+        for (let i = start; i <= end; i++) {
+            sum += i;
+        }
+        return sum;
+    } else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
