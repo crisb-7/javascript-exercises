711995d3bd92736959f11eb23baaf80517fca61f
crisb-7 <cristofer.becerra@protonmail.com>
2023-04-02 10:50:38 -0500

    Finish exercise 7. convertToFahrenheit function had celsius coefficient inverted xd.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,14 +1,14 @@
 const convertToCelsius = function(fahrenheit) {
   let celsius;
   celsius = (fahrenheit - 32)*(5/9);
-  // celsius = Math.round(celsius*10)/10;
+  celsius = Math.round(celsius*10)/10;
   return celsius;
 };
 
 const convertToFahrenheit = function(celsius) {
   let fahrenheit;
-  fahrenheit = celsius*(5/9) + 32;
-  // fahrenheit = Math.round(fahrenheit*10)/10
+  fahrenheit = celsius*(9/5) + 32;
+  fahrenheit = Math.round(fahrenheit*10)/10
   return fahrenheit;
 };
 
