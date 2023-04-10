3148f5065ec41751884103e7f381485b8239231e
crisb-7 <cristofer.becerra@protonmail.com>
2023-04-10 13:44:30 -0600

    Complete exercise 12, Find the oldest. All exercises complete with all tests passed.

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    let titles = [];
+    let lol = array.filter((object) => {
+        titles.push(object.title)});
+    return titles;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const oldest = people.sort((a, b) => {
+        const currentYear = (new Date()).getFullYear();
+        const ageA = "yearOfDeath" in a ? 
+                a.yearOfDeath - a.yearOfBirth : 
+                currentYear - a.yearOfBirth;
+        const ageB = "yearOfDeath" in b ?
+                    b.yearOfDeath - b.yearOfBirth :
+                    currentYear - b.yearOfBirth;
+        return ageA > ageB ? -1 : 1;
+    });
+    return people[0];
 };
 
 // Do not edit below this line
