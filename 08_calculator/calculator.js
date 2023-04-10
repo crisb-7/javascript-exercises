6f8c3c492316a6537dde4833428bcaece587313d
crisb-7 <cristofer.becerra@protonmail.com>
2023-04-09 20:27:56 -0600

    Complete exercise 8 - calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(firstNum, secondNum) {
+	return firstNum + secondNum;
 };
 
-const subtract = function() {
-	
+const subtract = function(firstNum, secondNum) {
+	return firstNum - secondNum;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  let compute = numbers.length > 0 ? numbers.reduce((total, number) => {return total + number}, 0) : 0
+	return compute;
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+	return numbers.reduce((total, number) => {return total*number}, 1);
 };
 
-const power = function() {
-	
+const power = function(...numbers) {
+	return numbers[0]**numbers[1];
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  if (number > 0) {
+    let fact = 1;
+    for (let i = number; i >= 1; i--) {
+      fact *= i;
+    }
+    return fact;
+  } else {
+    return 1;
+  }
 };
 
 // Do not edit below this line
