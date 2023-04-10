b137868eeea52ca344e7dd52d78aa69008800b0b
crisb-7 <cristofer.becerra@protonmail.com>
2023-04-09 21:08:45 -0600

    Complete exercise 10, fibonacci (ezpz, not memory efficient tho)

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if (number > 0) {
+        return calculateFibonacci(number);
+    } else {
+        return "OOPS";
+    }
 };
+
+function calculateFibonacci(num) {
+    let f = [0, 1];
+    for (let n = 2; n <= parseInt(num); n++) {
+        f[n] = f[n-1] + f[n-2];
+    }
+    return f[f.length - 1];
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
