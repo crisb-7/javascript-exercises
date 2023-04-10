285e8ce0909e1e3ee4e44b9dbe431943cbdafb99
crisb-7 <cristofer.becerra@protonmail.com>
2023-04-09 20:50:56 -0600

    Complete exercise 9, palindromes.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    text = normalizeString(text);
+    let inverted = "";
+    for (let i = text.length - 1; i >= 0; i--) {
+        inverted += text[i];
+    }
+    return text == inverted;
 };
+
+function normalizeString(str) {
+    str = str.toUpperCase();
+    str = str.toLowerCase();
+    str = str.replace(/[^\w]/g, "");
+    return str;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
